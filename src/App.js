d972f16709bed6d4fa6e915bd515c6f762b692b6
Sergey Shabanov <shsv382@gmail.com>
2024-02-01 21:18:45 +0300

    Added redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import Movies from './components/Movies';
 import { useState, useEffect } from 'react';
 import { APIKEY } from './env.js';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchData } from './actions';
+import { CircularProgress, Input } from '@mui/material';
 
 import './App.scss';
 
 function App() {
-  const [movies, setMovies] = useState([]);
+  // const [movies, setMovies] = useState([]);
   const [searchField, setSearchField] = useState('');
   const URL = `https://www.omdbapi.com/?s=${searchField}&apikey=${APIKEY}`;
+  const movies = useSelector(state => state.movies);
+  const dispatch = useDispatch();
 
   function getMovies() {
-    fetch(encodeURI(URL))
-      .then(res => res.json())
-      .then(movies => setMovies(movies.Search))
+    // fetch(encodeURI(URL))
+    //   .then(res => res.json())
+    //   .then(movies => setMovies(movies.Search))
+    dispatch(fetchData(URL))
   }
 
-  useEffect(getMovies, []);
+  useEffect(getMovies, [dispatch]);
   useEffect(getMovies, [searchField]);
   
   let timer;
@@ -29,8 +35,14 @@ function App() {
 
   return (
     <div className="App">
-      <input type="text" onChange={handleChange} />
-      <Movies movies={movies} />
+      <div>
+        <Input type="text" onChange={handleChange} />
+      </div>
+      {
+        movies.pending ? <CircularProgress /> :
+          movies.error ? movies.error :
+            <Movies movies={movies.movies} />
+      }
     </div>
   );
 }
